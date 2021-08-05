ffb7d8008ef55a51dde4269190e2e51830b4d27e
Roger K <misterhtmlcss@users.noreply.github.com>
2021-08-04 18:59:11 -0600

    Add more seed data

--- seeds.rb
+++ seeds.rb
@@ -1,17 +1,35 @@
-user_sharky = User.new({ username: "sharky_j", avatar_url: "http://naserca.com/images/sharky_j.jpg", email: "sharky_j@gmail.com", password: "abcd" })
+user_sharky = User.new({ username: "sharky_j", 
+                        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+                        email: "sharky@lhl.com",
+                        password: "abc123"})
 user_sharky.save
+sharky_fp = FinstagramPost.new({ photo_url: "http://naserca.com/images/shark.jpg",
+                                 user_id: user_sharky.id })
+sharky_fp.save
 
-finstagram_post_sharky = FinstagramPost.new({ photo_url: "http://naserca.com/images/shark.jpg", user_id: user_sharky.id })
-finstagram_post_sharky.save
 
-user_kirk = User.new({ username: "kirk_whalum", avatar_url: "http://naserca.com/images/kirk_whalum.jpg", email: "kirk_w@gmail.com", password: "abcd" })
+user_kirk = User.new({ username: "kirk_whalum", 
+                        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+                        email: "kirk@lhl.com",
+                        password: "abc123" })
 user_kirk.save
 
-finstagram_post_kirk = FinstagramPost.new({ photo_url: "http://naserca.com/images/whale.jpg", user_id: user_kirk.id })
-finstagram_post_kirk.save
+kirk_fp = FinstagramPost.new({ photo_url: "http://naserca.com/images/whale.jpg", user_id: user_kirk.id })
+kirk_fp.save
 
-user_peppa = User.new({ username: "marlin_peppa", avatar_url: "http://naserca.com/images/marlin_peppa.jpg", email: "marlin_p@gmail.com", password: "abcd" })
+
+user_peppa = User.new({ username: "marlin_peppa", 
+                        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+                        email: "peppa@lhl.com",
+                        password: "abc123" })
 user_peppa.save
 
-finstagram_post_peppa = FinstagramPost.new({ photo_url: "http://naserca.com/images/marlin.jpg", user_id: user_peppa.id })
-finstagram_post_peppa.save
+peppa_fp = FinstagramPost.new({ photo_url: "http://naserca.com/images/marlin.jpg", user_id: user_peppa.id })
+peppa_fp.save
+
+
+comment = Comment.new({ user_id: user_sharky.id, finstagram_post_id: sharky_fp.id, text: "this is awesome!" })
+comment.save
+
+like = Like.new({ user_id: user_sharky.id, finstagram_post_id: sharky_fp.id })
+like.save
